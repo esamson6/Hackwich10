06fd460f721eae120d40592170f9df9c030b6fbd
esamson6 <erin.samson34@yahoo.com>
2021-04-09 17:58:49 -1000

    Completed Problem Set 1 & 2

--- Hackwich10/ViewController.swift
+++ Hackwich10/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .lightGray
+        myLabel.text = ""
+        
     }
+    
+    @IBAction func segmentedControlPressed(_ sender: Any) {
 
-
+    switch segmentedControl.selectedSegmentIndex
+    
+    {
+        case 0:
+        let userInputText = textField.text
+            
+        self.myLabel.text = userInputText
+            view.backgroundColor = .blue
+    
+        case 1:
+        let userInputText = textField.text
+            
+        self.myLabel.text = userInputText
+            view.backgroundColor = .green
+    
+        case 2:
+        let userInputText = textField.text
+            
+        self.myLabel.text = userInputText
+            view.backgroundColor = .red
+    
+        default:
+           break
+            
+        }
+    }
 }
-
